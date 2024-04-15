ee60c423a8b2a4df03038d6b270d6174331aeab4
ianshih <ianshih2003@gmail.com>
2024-04-15 12:39:03 -0300

    Fix logging

---
+++ utils.py
@@ -0,0 +1,32 @@
+from configparser import ConfigParser
+import os
+
+
+def initialize_config():
+    """ Parse env variables or config file to find program config params
+
+    Function that search and parse program configuration parameters in the
+    program environment variables first and the in a config file.
+    If at least one of the config parameters is not found a KeyError exception
+    is thrown. If a parameter could not be parsed, a ValueError is thrown.
+    If parsing succeeded, the function returns a ConfigParser object
+    with config parameters
+    """
+
+    config = ConfigParser(os.environ)
+    # If config.ini does not exists original config object is not modified
+    config.read("config.ini")
+
+    config_params = {}
+    try:
+        config_params["token"] = config["DEFAULT"]["TOKEN"]
+        config_params["logging_level"] = os.getenv(
+            'LOGGING_LEVEL', config["DEFAULT"]["LOGGING_LEVEL"])
+        config_params["log_file"] = os.getenv(
+            'LOG_FILE', config["DEFAULT"]["LOG_FILE"])
+
+    except KeyError as e:
+        raise KeyError(
+            "Key was not found. Error: {} .Aborting server".format(e))
+
+    return config_params
